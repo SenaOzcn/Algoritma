6a3c6bd503efdfc76ba7df2a06d6222c8b98c7d1
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-04-28 18:19:11 +0300

    Create EkranaYazdirma.java

---
+++ AlgoritmalarıKodlamak/EkranaYazdirma.java
@@ -0,0 +1,9 @@
+// Ekrana sırasıyla 1, 2 ve 3 yazdıran Java Programı...
+
+class EkranaYazdırma {
+  public static void main(String[] Args) {
+    System.out.println("1");
+    System.out.println("2");
+    System.out.println("3");
+  }
+}
