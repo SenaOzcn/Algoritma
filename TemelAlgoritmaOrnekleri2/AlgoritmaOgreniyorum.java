36c51d65b4cb76ff31b63c88ee16ccd296d18cdc
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-04-29 15:40:14 +0300

    Create AlgoritmaOgreniyorum.java

---
+++ TemelAlgoritmaOrnekleri2/AlgoritmaOgreniyorum.java
@@ -0,0 +1,9 @@
+// Ekrana 10 kere "Algoritma Öğreniyorum!" yazdıran program...
+
+class AlgoritmaOgreniyorum {
+  public static void main(String[] Args) {
+    for (int i = 1; i <= 10; i++) {
+      System.out.println("Algoritma Öğreniyorum!");
+    }
+  }
+}
