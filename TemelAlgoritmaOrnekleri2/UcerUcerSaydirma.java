35cc9b95791770ac03316997d340100f86cd5656
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-04-29 17:30:03 +0300

    Create UcerUcerSaydirma.java

---
+++ TemelAlgoritmaOrnekleri2/UcerUcerSaydirma.java
@@ -0,0 +1,9 @@
+// 5'ten başlayıp 20'ye kadar 3'er 3'er artarak sayıları ekrana yazdıran program...
+
+class UcerUcerSaydirma {
+  public static void main(String[] Args) {
+    for (int i = 5; i <= 20; i += 3) {
+      System.out.println(i);
+    }
+  }
+}
