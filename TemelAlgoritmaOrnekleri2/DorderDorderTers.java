80d55357031268bcaa6e80dde831b7004b9e2e48
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-04-29 17:40:16 +0300

    Create DorderDorderTers.java

---
+++ TemelAlgoritmaOrnekleri2/DorderDorderTers.java
@@ -0,0 +1,9 @@
+// 20'den başlayıp, 1'e kadar 4'er 4'er geriye doğru sayarak ekrana yazdıran program...
+
+class DorderDorderTers {
+  public static void main(String[] Args) {
+    for (int i = 20; i >= 1; i -= 4) {
+      System.out.println(i);
+    }
+  }
+}
