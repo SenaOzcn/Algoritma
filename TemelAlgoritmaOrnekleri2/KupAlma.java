a3fe7ae836ac7998aaae7afb3720358c69f6bda2
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-04-29 17:50:38 +0300

    Create KupAlma.java

---
+++ TemelAlgoritmaOrnekleri2/KupAlma.java
@@ -0,0 +1,11 @@
+// 1'den 10'a kadar olan sayıların küplerini ekrana yazdıran program...
+
+import java.util.*;
+
+class KupAlma {
+  public static void main(String[] Args) {
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(Math.pow(i, 3));
+    }
+  }
+}
