77480578b2bce4c5ec17744db30763835bbdc7ae
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-03 21:10:40 +0300

    Create YuzKereYaziYazdirma.java

---
+++ TemelAlgoritmaOrnekleri3/YuzKereYaziYazdirma.java
@@ -0,0 +1,9 @@
+// Ekrana 100 kere "Merhaba Algoritma!" yazdıran program...
+
+class YuzKereYaziYazdirma {
+  public static void main(String args[]) {
+    for (int i = 1; i <= 100; i++) {
+      System.out.println("Merhaba Algoritma!");
+    }
+  }
+}
