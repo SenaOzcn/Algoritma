c4fc78e8c6807362361547b34871620573156c49
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-03 15:35:34 +0300

    Create ToplamCarpim.java

---
+++ TemelAlgoritmaOrnekleri3/ToplamCarpim.java
@@ -0,0 +1,17 @@
+// Kullanıcıdan iki sayı alıp, bu iki sayının farkını ve çarpımını ekrana yazdıran program...
+
+import java.util.*;
+
+class ToplamCarpim {
+  public static void main(String[] Args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Sayı1 => ");
+    int sayi1 = Integer.valueOf(scanner.nextLine());
+    System.out.print("Sayı2 => ");
+    int sayi2 = Integer.valueOf(scanner.nextLine());
+    int toplam = sayi1 + sayi2;
+    int carpim = sayi1 * sayi2;
+    System.out.println("Toplamları = " + toplam);
+    System.out.println("Çarpımları = " + carpim);
+  }
+}
