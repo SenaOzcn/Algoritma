8b173863d1936c844cd465e26b244bc14a8f01f5
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-03 21:42:53 +0300

    Create DokuzdanDoksanDokuza.java

---
+++ TemelAlgoritmaOrnekleri3/DokuzdanDoksanDokuza.java
@@ -0,0 +1,9 @@
+// 9'dan başlayıp 99'a kadar 11'er 11'er sayarak, sayıları ekrana yazdıran program...
+
+class DokuzdanDoksanDokuza {
+  public static void main(String args[]) {
+    for (int i = 9; i <= 99; i += 11) {
+      System.out.println(i);
+    }
+  }
+}
