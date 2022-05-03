4949d878017f40c1f4a36dfe96ece6e5330a7035
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-03 19:47:18 +0300

    Create MilKm.java

---
+++ TemelAlgoritmaOrnekleri3/MilKm.java
@@ -0,0 +1,13 @@
+// Deniz mili cinsinden verilen bir mesafenin kilometre cinsinden karşılığını bularak ekrana yazdıran program...
+
+import java.util.*;
+
+class MilKm {
+  public static void main(String args[]) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Deniz mili cinsinden mesafesiyi giriniz : ");
+    double mesafeMil = Double.valueOf(scanner.nextLine());
+    double mesafeKm = mesafeMil * 1.852;
+    System.out.println("Deniz mili cinsinden verilen bir mesafenin kilometre cinsinden karşılığı : " + mesafeKm);
+  }
+}
