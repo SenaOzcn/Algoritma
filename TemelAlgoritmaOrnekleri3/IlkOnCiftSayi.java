a47cc16f78cdd457bd592ea1ea8d4ea7911867f5
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-03 21:56:37 +0300

    Create IlkOnCiftSayi.java

---
+++ TemelAlgoritmaOrnekleri3/IlkOnCiftSayi.java
@@ -0,0 +1,9 @@
+// Sıfırdan büyük ilk 10 çift sayıyı ekrana yazdıran program...
+
+class IlkOnCiftSayi {
+  public static void main(String args[]) {
+    for (int i = 0; i <= 10; i+=2) {
+            System.out.println(i);
+    }
+  }
+}
